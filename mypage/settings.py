2b52a1c654bac5c857e0617f7c66a516de76a062
kamel-fedda <kamelsilver2005@gmail.com>
2024-09-06 22:33:41 +0100

    s

--- mypage/settings.py
+++ mypage/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k04o95!g)3r$@9cl9qu1o3o!kzd8s(!wzhh*fsh^bpibmc0ajh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Example of a global static directory
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
